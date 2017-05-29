b19619c40487d0fa8cdcfe5acb837bceb09dd548
sdowell <sdowell@umail.ucsb.edu>
2017-05-29 12:08:18 -0700

    Implemented 2PL transaction handling

--- 2PL.py
+++ 2PL.py
@@ -1,4 +1,4 @@
-
+import time
 
 class Operation:
 
@@ -9,17 +9,65 @@ class Operation:
 		self.key = key
 		# None if read, valid if write
 		self.value = value
+		# Indicates whether this operation holds the lock for its data item
+		self.hasLock = False
 		
 class Transaction:
 
 	def __init__(self, ops):
 		# list of operations for the transaction to perform
 		self.ops = ops
+		self.reads = t for t in ops if t.type == 'r'
+		self.writes = t for t in ops if t.type == 'w'
 
 class TwoPL:
 
 	def __init__(self):
 		return
 		
-	def initTransaction(self, T):
+	def initTransaction(self, T, db):
+		numOps = len(T.ops)
+		numLocks = 0
+		start = time.time()
+		# acquire locks
+		while numLocks != numOps:
+			# acquire locks
+			for op in T.ops:
+				if op.type == 'r' and not op.hasLock:
+					op.hasLock = db.get(op.key).acquire_RLock()
+					if op.hasLock:
+						numLocks += 1
+				elif op.type == 'w' and not op.hasLock:
+					op.hasLock = db.get(op.key).acquire_WLock()
+					if op.hasLock:
+						numLocks += 1
+			# timeout, release locks and start again after delay
+			if time.time() - start > 1:
+				for op in T.ops:
+					if op.type == 'r' and op.hasLock:
+						db.get(op.key).release_RLock()
+						op.hasLock = False
+						numLocks -= 1
+					elif op.type == 'w' and op.hasLock:
+						db.get(op.key).release_WLock()
+						op.hasLock = False
+						numLocks -= 1
+				time.sleep(0.01)
+				start = time.time()
+		# perform operations
+		for op in T.ops:
+			assert(op.hasLock)
+			if op.type == 'r':
+				op.value = db.get(op.key).value
+			elif op.type == 'w':
+				db.set(op.key, op.value)
+		
+		# release locks
+		for op in T.ops:
+			assert(op.hasLock)
+			if op.type == 'r':
+				db.get(op.key).release_RLock()
+			elif op.type == 'w':
+				db.get(op.key).release_WLock()
+			
 		return
