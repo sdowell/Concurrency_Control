4ac8d2b25e319be71b9b0dc548209e23f8f7f104
sdowell <sdowell@umail.ucsb.edu>
2017-05-29 11:37:56 -0700

    Added RW locks for database_items

--- db.py
+++ db.py
@@ -5,9 +5,44 @@ class Database_Item:
 	
 	def __init__(self, value):
 		self.value = value
-		self.r_lock = threading.Lock()
-		self.w_lock = threading.Lock()
+		#self.r_lock = threading.Lock()
+		self.lock = threading.Lock()
+		self.num_readers = 0
+		self.num_writers = 0
 
+	def acquire_RLock(self):
+		self.lock.acquire()
+		if self.num_writers == 0:
+			self.num_readers += 1
+			self.lock.release()
+			return True
+		else:
+			self.lock.release()
+			return False
+	def release_RLock(self):
+		self.lock.acquire()
+		assert(self.num_readers > 0)
+		self.num_readers -= 1
+		self.lock.release()
+		return True
+	
+	def acquire_WLock(self):
+		self.lock.acquire()
+		if self.num_readers == 0 and self.num_writers == 0:
+			self.num_writers += 1
+			self.lock.release()
+			return True
+		else:
+			self.lock.release()
+			return False
+		
+	def release_WLock(self):
+		self.lock.acquire()
+		assert(self.num_writers > 0)
+		self.num_writers -= 1
+		self.lock.release()
+		return True
+		
 class Database:
 
 	def __init__(self, size):
